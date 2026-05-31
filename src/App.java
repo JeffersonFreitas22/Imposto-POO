c300b41ea46b0d301a218a686b58b3972c2b8295
Jefferson <jeff.erson.meyt@gmail.com>
2026-05-30 21:38:59 -0300

    Refactor App to include user input for product details
    Updated the App class to read product details from user input and calculate taxes.

--- src/App.java
+++ src/App.java
@@ -1,19 +1,33 @@
+import java.util.Scanner;
 
 public class App {
 	
     public static void main(String[] args) {
+    	
+    	//Criar Scanner 
+    	Scanner in = new Scanner (System.in);
 
-        Produto p = new Produto();
+    	//Puxar dados de outra classe
+    	Produto p = new Produto();
+        Impostos i = new Impostos();
 
-        p.setNome("Borough Mid 2");
-        p.setValor(290.00);
+        //Lendo as infornações do produto
+        System.out.println("Nome do produto: ");
+        p.setNome(in.nextLine());
+        System.out.println("Valor do produto: ");
+        p.setValor(in.nextDouble());
+        
+        //Calcular valor liquido
+        Double total = p.getValor() + i.calcularIcms(p) + i.calcularIss(p) + i.calcularIpi(p);
 
-        Impostos imp = new Impostos();
-
+        //Apresentar valores
         System.out.println("Produto: " + p.getNome());
-        System.out.println("Preço Original: R$ " + p.getValor());
-
-        imp.Icms(p);
-
+        System.out.println("Valor bruto: " + p.getValor());
+        System.out.println("Icms: " + i.calcularIcms(p));
+        System.out.println("Iss: " +i.calcularIss(p));
+        System.out.println("Ipi: " +i.calcularIpi(p));
+        System.out.println("Valor liquido: " + total);
+        
+        in.close();
     }
 }
