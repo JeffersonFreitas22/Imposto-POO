98fda13664e8cdb7a0880bf7ae95826f6251b4da
Jefferson <jeff.erson.meyt@gmail.com>
2026-05-30 21:39:18 -0300

    Refactor Impostos class to use calculation methods

--- src/Impostos.java
+++ src/Impostos.java
@@ -1,31 +1,18 @@
 
 public class Impostos {
-	Produto p = new Produto();
-	private Double iss;
-	public void Icms (Produto produto) {
-		
-		Double A = p.getValor() * 18 / 100;
-		Double icms = p.getValor() - A;
-		
-		System.out.println("ICMS: " +icms);
+	
+	public Double calcularIcms (Produto p) {
+		Double icms = p.getValor() * 0.18;
+		return icms;
 	}
 	
-	
-	public void Iss (Produto produto) {
-		Double B = p.getValor() * 5 / 100;
-		iss = p.getValor() - B;
-		
+	public Double calcularIss (Produto p) {
+		Double iss = p.getValor() * 0.05;
+		return iss;
 	}
 	
-	
-	public void Ipi (Produto produto) {
-		Double C = p.getValor() * 10 / 100;
-		Double ipi = p.getValor() - C;
-
-	}
-	
-	
-	public void ValorT (Produto produto) {
-		Double valorTotal = p.getValor();
+	public Double calcularIpi (Produto p) {
+		Double ipi = p.getValor() * 0.1;
+		return ipi;
 	}
 }
